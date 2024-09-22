e81f0a37aff2bd820b1fba5d2d42d03434348be7
jung jinyoung <ya_1001@naver.com>
2024-09-22 14:43:32 +0900

    240922 리액트 node.js 강의 완료

--- REACT/chapter03/src/index.js
+++ REACT/chapter03/src/index.js
@@ -1,1 +1,4 @@
-console.log("안녕 Node js?");
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
---
+++ REACT/chapter03/src/math.js
@@ -0,0 +1,29 @@
+// Math 모듈
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function sub(a, b) {
+//   return a - b;
+// }
+
+// CJS
+// module.exports = {
+//   add,
+//   sub,
+// };
+
+// ESM
+// export { add, sub };
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+export default function multifly(a, b) {
+  return a * b;
+}
